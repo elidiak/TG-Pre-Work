6d26c25890b190aa9b77ba29ee8eea3855cfba29
Eric G. Lidiak <eric@lidiak.com>
2018-10-31 17:06:25 -0600

    Completed Lesson: Conditional Statements

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/The-switch-keyword.js
@@ -0,0 +1,19 @@
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+    //First place
+    case 'first place' :
+    console.log('You get the gold medal!');
+    break;
+    //Second Place
+    case 'second place' :
+    console.log('You get the silver medal!');
+    break;
+    //Third Place
+    case 'third place' :
+    console.log('You get the bronze medal!');
+    break;
+    //Default case
+  default:
+    console.log('No medal awarded.');
+    break;
+}
