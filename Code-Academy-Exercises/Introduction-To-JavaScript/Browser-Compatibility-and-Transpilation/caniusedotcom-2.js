420b9f056a351d224d3a847a534b9d69464111e9
Eric G. Lidiak <eric@lidiak.com>
2018-11-02 15:05:57 -0600

    Completed Lesson: Browser Compatibility

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,6 @@
+// Set the variable below to a number
+let esFivePercentageSupport;
+esFivePercentageSupport = 96.29;
+// Set the variable below to a number
+let esSixTemplateLiterals;
+esSixTemplateLiterals = 91.75;
